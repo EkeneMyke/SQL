2a60d31509981d77bc0d0d433d585af7f595e469
Ekene Richard <122648370+EkeneMyke@users.noreply.github.com>
2023-09-06 13:56:44 +0100

    Add files via upload

---
+++ Create Tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE sales (
+		customer_ID VARCHAR(1),
+        order_date DATE,
+        product_ID int
+);
+
+CREATE TABLE members (
+		customer_ID VARCHAR(1),
+        join_date TIMESTAMP
+);
+
+CREATE TABLE menu (
+		product_ID INT,
+        product_name VARCHAR(5),
+        price INT
+);
+        
+        
